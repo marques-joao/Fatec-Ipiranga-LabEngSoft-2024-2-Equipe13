653f59f80543fbb7a6c9f7342909a16f9d1e6a0c
Joao Marques <jvitormarques79@gmail.com>
2024-11-20 15:14:24 -0300

    feat: inclui perfil de producao

--- labEngSoft-Entregas-Sprint#5/backend/src/main/java/com/joaomarques/teste_vitalcare/domain/repository/EventoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.joaomarques.teste_vitalcare.domain.repository;
-
-import com.joaomarques.teste_vitalcare.domain.entity.EventoEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EventoRepository extends JpaRepository<EventoEntity, Long> {}
